b12d78844317df816851f70362002cac81fae0c7
MrVineetRaj <vineetrajrj26@gmail.com>
2025-07-31 21:50:50 +0530

    Razorpay subscription cycle

---
+++ src/app/api/webhooks/razorpay/route.ts
@@ -0,0 +1,57 @@
+import { NextRequest, NextResponse } from "next/server";
+import crypto from "crypto";
+import { db } from "@/lib/prisma";
+import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils";
+import { PaymentStatus } from "@/generated/prisma";
+import { razorpayActions } from "@/modules/razorpay/utils";
+
+export async function POST(req: NextRequest) {
+  // Todo : implement flow for updating subscription access
+  const data = await req.json();
+  console.log(data);
+  const signatureFromRazorPay = req.headers.get("X-Razorpay-Signature");
+
+  if (!signatureFromRazorPay) {
+    return NextResponse.json({
+      status: 401,
+      message: "Signature not found",
+    });
+  }
+  const RAZORPAY_WEBHOOK_SECREt = process.env.RAZORPAY_WEBHOOK_SECREt!;
+
+  const isValid = validateWebhookSignature(
+    JSON.stringify(data),
+    signatureFromRazorPay,
+    RAZORPAY_WEBHOOK_SECREt
+  );
+
+  if (!isValid) {
+    console.log("false request");
+    console.log(data);
+    return NextResponse.json({
+      status: 401,
+      data,
+    });
+  }
+  
+  if (
+    data.contains.include("subscription") &&
+    data.contains.include("payment")
+  ) {
+    razorpayActions.upsertSubscriptionDetails(
+      true,
+      data.payload.subscription.entity,
+      data.payload.payment.entity
+    );
+  } else if (data.contains.include("subscription")) {
+    razorpayActions.upsertSubscriptionDetails(
+      false,
+      data.payload.subscription.entity
+    );
+  }
+
+  return NextResponse.json({
+    status: "OK",
+    data,
+  });
+}
--- src/modules/razorpay/utils.ts
+++ src/modules/razorpay/utils.ts
@@ -1,4 +1,8 @@
+import { PaymentStatus, SubscriptionStatus } from "@/generated/prisma";
+import { db } from "@/lib/prisma";
 import Razorpay from "razorpay";
+import { Payments } from "razorpay/dist/types/payments";
+import { Subscriptions } from "razorpay/dist/types/subscriptions";
 
 export class RazorpayInstance {
   razorpayInstance: Razorpay;
@@ -16,4 +20,78 @@ export class RazorpayInstance {
   }
 }
 
+export class RazorpayActions {
+  async upsertSubscriptionDetails(
+    isPaymentIncluded: boolean,
+    subscription: Subscriptions.RazorpaySubscription,
+    payment?: Payments.RazorpayPayment
+  ) {
+    const subscriptionFromDb = await db.subscription.findUnique({
+      where: {
+        subscriptionId: subscription.id,
+      },
+    });
+    await db.subscription.update({
+      where: {
+        subscriptionId: subscription.id,
+      },
+      data: {
+        planId: subscription.plan_id,
+        status: subscription.status,
+        cycleStart: subscription.current_start
+          ? new Date(+subscription.current_start * 1000)
+          : null,
+        cycleEnd: subscription.current_start
+          ? new Date(+subscription.current_start * 1000)
+          : null,
+        upcomingPayment: subscription.charge_at
+          ? new Date(+subscription.charge_at * 1000)
+          : null,
+      },
+    });
+
+    if (isPaymentIncluded && payment) {
+      await db.payments.upsert({
+        where: {
+          paymentId: payment.id,
+        },
+        update: {
+          updatedAt: new Date(),
+        },
+        create: {
+          paymentId: payment.id,
+          orgname: subscriptionFromDb?.orgname!,
+          username: subscriptionFromDb?.username!,
+          amount: +payment.amount,
+          currency: payment.currency,
+          status: payment.status,
+          invoiceId: payment.invoice_id ? payment.invoice_id : null,
+          orderId: payment.order_id,
+          method: payment.method,
+          paymentDetails: (payment as any)[payment.method],
+          email: payment.email,
+          contact: String(payment.contact),
+        },
+      });
+    }
+  }
+}
 export const razorpayInstance = new RazorpayInstance();
+export const razorpayActions = new RazorpayActions();
+
+/**
+ *   paymentId String @id
+  orgname String 
+  username String 
+  amount Int
+  currency String
+  status PaymentStatus @default(pending)
+  invoiceId String 
+  orderId String 
+  method String
+  paymentDetails Json 
+  email String
+  contact String
+
+
+ */
